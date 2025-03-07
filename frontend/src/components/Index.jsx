457344a624ae09d599e731b6fa2fb3dd1c4ff861
Manuel <joseguns2@gmail.com>
2025-03-06 22:53:10 -0800

    Actualizando

--- frontend/src/components/Index.jsx
+++ frontend/src/components/Index.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Letter from "./Letter";
 import SearchBar from "./SearchBar";
 import OrderList from "./OrderList";
@@ -7,6 +8,24 @@ import CreateViga from "./CreateVigas";
 
 export default function Inicio() {
   const [view, setView] = useState("index");
+  const [datos, setDatos] = useState([]);
+  const [cargando, setCargando] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/api/fecha/")
+      .then((respuesta) => {
+        setDatos(respuesta.data);
+        setCargando(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setCargando(false);
+      });
+  }, []);
+  console.log(datos);
+
   return (
     <div className="p-6">
       <Letter setView={setView} view={view} />
