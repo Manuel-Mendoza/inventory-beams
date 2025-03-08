e999bca37a8560b3ae3ac66b616d7331a6a6129a
Manuel <joseguns2@gmail.com>
2025-03-08 15:28:12 -0800

    Update Input.jsx

--- frontend/src/components/Maquetado/Input.jsx
+++ frontend/src/components/Maquetado/Input.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 
-const Input = ({ placeholder, type, style, inputmode, onChange }) => {
+const Input = ({value, placeholder, type, style, inputmode, onChange }) => {
   return (
     <input
+    value={value}
       className={`outline-0 p-2 border rounded w-full ${style}`}
       type={type}
       placeholder={placeholder}
