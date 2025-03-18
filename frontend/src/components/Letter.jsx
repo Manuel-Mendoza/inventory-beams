d8923daa9d8cc67b6de8266ea530e50124738fe5
Manuel <joseguns2@gmail.com>
2025-03-17 20:48:59 -0700

    Update Letter.jsx

--- frontend/src/components/Letter.jsx
+++ frontend/src/components/Letter.jsx
@@ -9,44 +9,8 @@ export default function Letter({ setView, view }) {
   };
 
   return (
-    <div className="flex justify-between items-center mb-4">
-      <div className="flex items-center">
-        <h1 className="text-2xl font-bold text-gray-800">Inventory Beams</h1>
-        <div className="ml-6 space-x-4">
-          <button
-            onClick={() => setView("index")}
-            className={`px-4 py-2 rounded-md ${
-              view === "index"
-                ? "bg-blue-500 text-white"
-                : "bg-gray-200 text-gray-700 hover:bg-gray-300"
-            }`}
-          >
-            Orders 🗒
-          </button>
-          <button
-            onClick={() => setView("orden")}
-            className={`px-4 py-2 rounded-md ${
-              view === "orden"
-                ? "bg-blue-500 text-white"
-                : "bg-gray-200 text-gray-700 hover:bg-gray-300"
-            }`}
-          >
-            New Order ➕
-          </button>
-          <button
-            onClick={() => setView("edit")}
-            className={`px-4 py-2 rounded-md ${
-              view === "edit"
-                ? "bg-blue-500 text-white"
-                : "bg-gray-200 text-gray-700 hover:bg-gray-300"
-            }`}
-          >
-            Edit Order ✏️
-          </button>
-        </div>
-      </div>
-
-      <div className="flex items-center">
+    <div className="flex gap-4 max-md:flex-col justify-between items-center mb-4 p-2">
+      <div className="flex items-center justify-between w-full">
         <span className="mr-4 text-gray-600">
           Usuario: {username || "Admin"}
         </span>
@@ -56,6 +20,41 @@ export default function Letter({ setView, view }) {
         >
           Cerrar Sesión
         </button>
+      </div>
+      <div className="flex items-center max-md:grid gap-2">
+        <h1 className="text-2xl font-bold text-gray-800">Inventory Beams</h1>
+        <div className="ml-6 space-x-4 max-md:grid gap-2">
+          <button
+            onClick={() => setView("index")}
+            className={`px-4 py-2 rounded-md mr-0 w-full ${
+              view === "index"
+                ? "bg-blue-500 text-white"
+                : "bg-gray-200 text-gray-700 hover:bg-gray-300"
+            }`}
+          >
+            Orders 🗒
+          </button>
+          <button
+            onClick={() => setView("orden")}
+            className={`px-4 py-2 rounded-md mr-0 w-full ${
+              view === "orden"
+                ? "bg-blue-500 text-white"
+                : "bg-gray-200 text-gray-700 hover:bg-gray-300"
+            }`}
+          >
+            New Order ➕
+          </button>
+          <button
+            onClick={() => setView("edit")}
+            className={`px-4 py-2 rounded-md w-full ${
+              view === "edit"
+                ? "bg-blue-500 text-white"
+                : "bg-gray-200 text-gray-700 hover:bg-gray-300"
+            }`}
+          >
+            Edit Order ✏️
+          </button>
+        </div>
       </div>
     </div>
   );
